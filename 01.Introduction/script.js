4ece595db6f56b24f695587e73df97d94f326e52
Hajizada Aykhan <aykhanih@code.local>
2024-05-12 14:45:05 +0400

    Introduction

---
+++ 01.Introduction/script.js
@@ -0,0 +1,65 @@
+
+// let firstName = 'Aykhan'
+// let lastName = 'Hajizada'
+// let age = '25'
+// let fatherName = 'Isa'
+
+// let fullInfo = `First Name: ${firstName} 
+// Surname: ${lastName} 
+// Father Name: ${fatherName}
+// Age: ${age}`
+
+
+
+
+// console.log(fullInfo);
+
+
+// let a = 1
+// let b = '1'
+// let c = 1
+
+// console.log(a+b+c);
+// console.log(b+a+c);
+// console.log(c+b+a);
+// console.log(c+a+b);
+
+
+// console.log(a*b*c);
+
+
+
+let age = '24'
+let name = 'Ayxan'
+let surname = 'Hajizada'
+
+
+
+//1
+
+if (age == 23) {
+    console.log('1');
+}
+else if (age == '23' && name == 'Ayxan') {
+    console.log('2');
+}
+else if (age == '25' && name !== "Ulfet" && surname !== 'Zakirli')
+    console.log('3');
+else {
+    console.log('4');
+}
+
+
+//2
+
+if (age === 23 || name !== "Ulfat" || surname !== 'Zakirli') {
+    console.log('1');
+}
+else if (name === 'Ayxan') {
+    console.log('2');
+}
+else if (surname === 'Hajizada')
+    console.log('3');
+else {
+    console.log('4');
+}
